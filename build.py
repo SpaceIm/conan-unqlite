2211ca20a7830b6914ac0bfa5d46878080a950d1
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-08-15 23:54:41 +0200

    fix again

--- build.py
+++ build.py
@@ -2,5 +2,5 @@ from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="SpaceIm", channel="testing", build_policy="missing", skip_check_credentials=True)
-    builder.add_common_builds(pure_c=True, dll_with_static_runtime=True, build_all_options_values=None)
+    builder.add_common_builds(reference="unqlite/1.1.9@SpaceIm/testing", pure_c=True, dll_with_static_runtime=True, build_all_options_values=None)
     builder.run()
