eaf65cc5e3b0810132d83831281f41576fe6f311
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-08-15 23:28:55 +0200

    try to fix CI again

--- build.py
+++ build.py
@@ -1,5 +1,6 @@
-from bincrafters import build_template_default
+from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = build_template_default.get_builder(pure_c=True, build_policy="missing")
+    builder = ConanMultiPackager(username="SpaceIm", channel="testing", build_policy="missing", skip_check_credentials=True)
+    builder.add_common_builds(pure_c=True, dll_with_static_runtime=True, build_all_options_values=None)
     builder.run()
